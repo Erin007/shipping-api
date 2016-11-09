10536d1e3fcd51dc4fab37fec6e31e9ec9b6554a
Erin Bond <erin@erinebond.com>
2016-11-08 17:33:55 -0800

    UPS and USPS hardcoded rates found and rendered in json

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -8,17 +8,28 @@ class ShipmentsController < ApplicationController
                                       city: 'Beverly Hills',
                                       zip: '90210')
 
-    destination = ActiveShipping::Location.new(country: 'CA',
-                                           province: 'ON',
-                                           city: 'Ottawa',
-                                           postal_code: 'K1P 1J1')
+    destination = ActiveShipping::Location.new(country: 'US',
+                                              state: 'WA',
+                                              city: 'Seattle',
+                                              zip: '98136'
+                                           )
 
     usps = ActiveShipping::USPS.new(login: '677JADED7283')
+
     response = usps.find_rates(origin, destination, [package])
 
-    render json: response
+    usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    ups = ActiveShipping::UPS.new(login: 'shopifolk', password: 'Shopify_rocks', key: '7CE85DED4C9D07AB')
+    response = ups.find_rates(origin, destination, [package])
+    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+
+    all_rates = usps_rates + ups_rates
+    render json: all_rates
   end
 
 end
+
+ #ENV["ACTIVESHIPPING_USPS_LOGIN"]
 
 #Do this for each of the postal services you want to use, include the params for each of the things and hide the secrets and keys in the .env file
