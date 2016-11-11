16e8a94538ec0d199d36e741133c480f6b76484e
jm-rives <joanna.rives@gmail.com>
2016-11-11 14:34:29 -0800

    ADD working controller test for destination

--- test/controllers/shipments_controller_test.rb
+++ test/controllers/shipments_controller_test.rb
@@ -2,11 +2,18 @@ require 'test_helper'
 
 class ShipmentsControllerTest < ActionDispatch::IntegrationTest
 
-  test "Require fields must be present in origin" do
+  test "Required fields must be present in origin" do
     origin = ActiveShipping::Location.new
     fields = %w(country city state address)
     
     assert_not_nil origin, fields
+  end
+
+  test "Required fields must be present in destination" do
+    destination = ActiveShipping::Location.new
+    fields = %w(country city state address)
+    
+    assert_not_nil destination, fields
   end
 
 end
