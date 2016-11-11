d1cb0049920c6a4df573acdfacdada531518de68
jm-rives <joanna.rives@gmail.com>
2016-11-11 14:18:12 -0800

    ADD working controller test

--- test/controllers/shipments_controller_test.rb
+++ test/controllers/shipments_controller_test.rb
@@ -1,34 +1,12 @@
 require 'test_helper'
 
 class ShipmentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get shipments_index_url
-    assert_response :success
-  end
 
-  test "should get show" do
-    get shipments_show_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get shipments_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get shipments_create_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get shipments_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get shipments_destroy_url
-    assert_response :success
+  test "Require fields must be present in origin" do
+    origin = ActiveShipping::Location.new
+    fields = %w(country city state address)
+    
+    assert_not_nil origin, fields
   end
 
 end
