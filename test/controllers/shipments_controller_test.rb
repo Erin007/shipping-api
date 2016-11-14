1cd365db32466bfa2b1291c9e9158d1bb3eb7382
Erin Bond <erin@erinebond.com>
2016-11-13 21:27:46 -0800

    Note that only required fields are zip and country for origin and destination

--- test/controllers/shipments_controller_test.rb
+++ test/controllers/shipments_controller_test.rb
@@ -1,18 +1,18 @@
 require 'test_helper'
 
 class ShipmentsControllerTest < ActionDispatch::IntegrationTest
-
+#I think the only required fields are the zip and the country, the shipping apis can figure out the rest from those two pieces of information...
   test "Required fields must be present in origin" do
     origin = ActiveShipping::Location.new
     fields = %w(country city state address)
-    
+
     assert_not_nil origin, fields
   end
 
   test "Required fields must be present in destination" do
     destination = ActiveShipping::Location.new
     fields = %w(country city state address)
-    
+
     assert_not_nil destination, fields
   end
 
